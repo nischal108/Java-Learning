820c6e98a3b3b52eba4b179a1722c80a7fbf8307
Nischal Bhattarai <nishchalb21@gmail.com>
2024-01-28 21:55:09 +0545

    frequency array concepts

---
+++ JavaJourney100/day2/checkingIftheNumberisPresent.java
@@ -0,0 +1,66 @@
+/*  searching for a number in an array and displaying whether a number is present 
+ * and if present how many times is it present content of the array is between 0 and 1000000
+ * the program must be time efficient
+ * 
+ * 
+ *      Logic used :
+ * as soon as the user inputs and array the array is traversed and a frequency array is made  of the max size defined 
+ * the freq array stores the freq of repetition of the content of array so when the ask we don't have to traverse the array.
+*/
+
+
+
+package JavaJourney100.day2;
+import java.util.Scanner;
+
+public class checkingIftheNumberisPresent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        arrayInput(array,sc);
+
+        int[] freqarray = frequencyarray(array);
+
+        System.out.print("Enter how many queries do you have");
+        int queries = sc.nextInt();
+
+        while (queries>0) {
+            System.out.print("Enter the number to search for : ");
+            int num = sc.nextInt();
+            if(freqarray[num]>0){
+                System.out.println("Yes number is present ");
+            }
+            else{
+                System.out.println("no number is not present ");
+            }
+         queries--;   
+        }
+
+
+        sc.close();
+    }
+
+
+    static void arrayInput( int[] array, Scanner sc){
+        for(int i =0; i<array.length ; i++){
+            System.out.print("Enter " +(i+1) + " item : ");
+            array[i] = sc.nextInt();
+        }
+    }
+
+    static int[] frequencyarray (int [] array){
+        int[] freq = new int[100000];
+        for(int i=0;i<array.length;i++){
+            freq[array[i]]++;
+        }
+
+        return freq;
+    }
+    
+}
---
+++ JavaJourney100/day2/reversingByKSteps.java
@@ -0,0 +1,73 @@
+package JavaJourney100.day2;
+
+import java.util.Scanner;
+
+public class reversingByKSteps{
+
+
+    //method to take input from the user
+    static void inputArray(int[]array, Scanner sc){
+        int length = array.length;
+
+        for(int i =0;i<length ;i++){
+            System.out.print("Enter " +(i+1)+" element : ");
+            array[i]=sc.nextInt();
+        }
+    }
+
+
+    //method to swap 
+    static void swap(int[] arr, int i,int  j){
+        int temp =arr[i];
+        arr[i]= arr[j];
+        arr[j]= temp;
+    }
+  
+
+    // method to reverse an array
+
+    static void reverseArray(int[]array,int i,int j){
+        while (i<j){
+            swap(array,i,j);
+            i++;
+            j--;
+        }
+    }
+
+
+
+    //method to display array 
+
+    static void displayArray(int[] arr){
+        int length = arr.length;
+        for(int i =0;i<length ;i++){
+            System.out.print( arr[i] + " " );
+        }
+    }
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array  : ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        //calling the method to ask for input
+        inputArray(array,sc);
+        System.out.println("Enter the value of k : ");
+
+        int kReal = sc.nextInt();
+
+        int k= kReal % size;
+
+
+        reverseArray(array, 0, size - 1); // Reverse the entire array
+        reverseArray(array, 0, k - 1); // Reverse the first k elements
+        reverseArray(array, k, size - 1); // Reverse the remaining elements
+
+
+        System.out.println("Finally array after reversing it by " + kReal + " steps");
+        displayArray(array);
+    }
+}
